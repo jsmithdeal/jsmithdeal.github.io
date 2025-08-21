fcda8e0c2abc77e10073126a3e096b3d471a3ca4
John Paul Smithdeal <johnpaulsmithdeal@gmail.com>
2025-08-21 17:14:43 -0400

    conver to pure JS/html SPA

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+    var currPage = sessionStorage.getItem("refreshUrl");
+
+    if (currPage == null || currPage == "")
+        navigate("html/home.html");
+    else
+        navigate(currPage);
+
     $(".navbar-toggler").on("click", function(){
         var btn = $(this);
         var icon = $(".navbar-toggler-icon");
@@ -9,3 +16,18 @@ $(document).ready(function(){
             icon.css("background-image", "url(images/hamburger.svg)");
     });
 });
+
+function navigate(fileUrl) {
+    sessionStorage.setItem("refreshUrl", fileUrl);
+
+    if (history.state == null || fileUrl != history.state.stateFileUrl)
+        history.pushState({ stateFileUrl: fileUrl }, '', 'index.html');
+    
+    $("#wrapper").html("");
+    $("#wrapper").load(history.state.stateFileUrl);
+}
+
+window.onpopstate = function (e) {
+    sessionStorage.setItem("refreshUrl", e.state.stateFileUrl);
+    $("#wrapper").load(e.state.stateFileUrl);
+}
