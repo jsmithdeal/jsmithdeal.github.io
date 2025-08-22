ac95aac27630ef6af2263381b5c01ab95275be4a
John Paul Smithdeal <johnpaulsmithdeal@gmail.com>
2025-08-22 19:35:24 -0400

    many css and html updates for organization and consistency. Implemented experience page

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 
     $(".navbar-toggler").on("click", function(){
         var btn = $(this);
-        var icon = $(".navbar-toggler-icon");
+        var icon = btn.find(".navbar-toggler-icon");
 
         if (!btn.hasClass("collapsed"))
             icon.css("background-image", "url(images/close.svg)");
