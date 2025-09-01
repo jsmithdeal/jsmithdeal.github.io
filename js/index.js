167d45fea9507c4026d88bcad83e946f4930b7c5
John Paul Smithdeal <johnpaulsmithdeal@gmail.com>
2025-09-01 11:02:10 -0400

    some comments and verbiage updates

--- js/index.js
+++ js/index.js
@@ -1,61 +1,75 @@
+//Do these things when the document is ready
 $(document).ready(function(){
-    var currPage = sessionStorage.getItem("refreshUrl");
+    try {
+        var currPage = sessionStorage.getItem("refreshUrl");
 
-    if (currPage == null || currPage == "")
-        navigate("html/home.html");
-    else
-        navigate(currPage);
-
-    $(".navbar-toggler").on("click", function(){
-        var btn = $(this);
-        var icon = btn.find(".navbar-toggler-icon");
-
-        if (!btn.hasClass("collapsed"))
-            icon.css("background-image", "url(images/close.svg)");
+        //If current page hasn't been set, go to home by default
+        if (currPage == null || currPage == "")
+            navigate("html/home.html");
         else
-            icon.css("background-image", "url(images/hamburger.svg)");
-    });
+            navigate(currPage);
 
-    if (!$("navbar-toggler").hasClass("collapsed"))
-        $("navbar-toggler").addClass("collapsed");
+        //Change mobile menu button from hamburger to x on menu open
+        $(".navbar-toggler").on("click", function(){
+            var btn = $(this);
+            var icon = btn.find(".navbar-toggler-icon");
+
+            if (!btn.hasClass("collapsed"))
+                icon.css("background-image", "url(images/close.svg)");
+            else
+                icon.css("background-image", "url(images/hamburger.svg)");
+        });
+
+        if (!$("navbar-toggler").hasClass("collapsed"))
+            $("navbar-toggler").addClass("collapsed");
+    }
+    catch (error){
+        console.log(error);
+    }
 });
 
+//Navigate to a new page (i.e. inject new page code into index wrapper)
 function navigate(fileUrl) {
-    var nav = $("#navbarNav");
-    var fileName = fileUrl.replace("html/", "").replace(".html", "");
+    try {
+        var nav = $("#navbarNav");
+        var fileName = fileUrl.replace("html/", "").replace(".html", "");
 
-    sessionStorage.setItem("refreshUrl", fileUrl);
+        sessionStorage.setItem("refreshUrl", fileUrl);
 
-    if (history.state == null || fileUrl != history.state.stateFileUrl)
-        history.pushState({ stateFileUrl: fileUrl }, '', 'index.html');
-    
-    $("#wrapper").html("");
-    $("#wrapper").load(history.state.stateFileUrl);
+        //Push current navigation target to state for handling back and forward clicks (in onpopstate)
+        if (history.state == null || fileUrl != history.state.stateFileUrl)
+            history.pushState({ stateFileUrl: fileUrl }, '', 'index.html');
+        
+        //Inject new html
+        $("#wrapper").html("");
+        $("#wrapper").load(history.state.stateFileUrl);
 
-    if (nav.hasClass("show"))
-    {
-        nav.removeClass("show");
-        $(".navbar-toggler-icon").css("background-image", "url(images/hamburger.svg)");
+        //Auto-close mobile menu bar on navigate
+        if (nav.hasClass("show"))
+        {
+            nav.removeClass("show");
+            $(".navbar-toggler-icon").css("background-image", "url(images/hamburger.svg)");
+        }
+
+        track(fileName);
+        updateActive(fileName);
     }
-
-    //track(fileName); // DO NOT CHECK IN - commented locally so I don't mess with the visitor count while testing
-    updateActive(fileName);
+    catch (error){
+        console.log(error);
+    }
 }
 
+//Counts unique site/page visits using a counter implemented via cloudflare workers
 async function track(fileName){
     try {
+        //If cookie doesn't exist then increase counter
         if (fileName != null && fileName != "" && document.cookie.indexOf(`${fileName}=true`) == -1){
-            try {
-                if (fileName == "home")
-                    await fetch(`https://portfolio.johnpaulsmithdeal.workers.dev?type=total`);
+            if (fileName == "home")
+                await fetch(`https://portfolio.johnpaulsmithdeal.workers.dev?type=total`);
 
-                await fetch(`https://portfolio.johnpaulsmithdeal.workers.dev?type=${fileName}`);
+            await fetch(`https://portfolio.johnpaulsmithdeal.workers.dev?type=${fileName}`);
 
-                document.cookie = `${fileName}=true; expires=Fri, 31 Dec 2038 12:00:00 UTC`;
-            }
-            catch (error){
-                console.log(error);
-            }
+            document.cookie = `${fileName}=true; expires=Fri, 31 Dec 2038 12:00:00 UTC`;
         }
     }
     catch (error){
@@ -63,25 +77,43 @@ async function track(fileName){
     }
 }
 
+//Navigation handling for site links
 function navigateLink(target){
-    if (target == "li")
-        window.open("https://www.linkedin.com/in/john-paul-smithdeal-b50293102", "_blank").focus();
-    else if (target == "gh")
-        window.open("https://github.com/jsmithdeal/jsmithdeal.github.io", "_blank").focus();
-}
-
-function updateActive(fileName) {
-    var targetLink = $(`.nav-item .${fileName}`);
-
-    $(".nav-link").removeClass("active");
-
-    if (targetLink != null){
-        targetLink.addClass("active");
+    try {
+        if (target == "li")
+            window.open("https://www.linkedin.com/in/john-paul-smithdeal-b50293102", "_blank").focus();
+        else if (target == "gh")
+            window.open("https://github.com/jsmithdeal/jsmithdeal.github.io", "_blank").focus();
+    }
+    catch (error){
+        console.log(error);
     }
 }
 
+//Updates menu items when one is clicked
+function updateActive(fileName) {
+    try {
+        var targetLink = $(`.nav-item .${fileName}`);
+
+        $(".nav-link").removeClass("active");
+
+        if (targetLink != null){
+            targetLink.addClass("active");
+        }
+    }
+    catch (error){
+        console.log(error);
+    }
+}
+
+//Handles loading correct html on back/forward click
 window.onpopstate = function (e) {
-    sessionStorage.setItem("refreshUrl", e.state.stateFileUrl);
-    $("#wrapper").load(e.state.stateFileUrl);
-    updateActive(e.state.stateFileUrl);
+    try {
+        sessionStorage.setItem("refreshUrl", e.state.stateFileUrl);
+        $("#wrapper").load(e.state.stateFileUrl);
+        updateActive(e.state.stateFileUrl);
+    }
+    catch (error){
+        console.log(error);
+    }
 }
